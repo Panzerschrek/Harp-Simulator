ecb0109d74efe1102152669bc4fdd923e6e5208f
Panzerschrek <AntiPanzerschrek@yandex.ru>
2017-12-16 18:44:52 +0700

    Nacat perehod na biblioteku SDL. Sozdajotsä okno, obrabatyvajetsä vvod, koje-cto uže risujetsä.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,36 @@ std::vector<int> mix_buffer_;
 SDL_Window* window_= nullptr;
 SDL_Surface* surface_= nullptr;
 
+// View
+const unsigned int c_window_width= 400;
+const unsigned int c_window_height= 144;
+
+const unsigned int c_harp_apertures_count= 10;
+const unsigned int c_harp_aprture_modes_count= 2;
+
+const unsigned int c_edge_border_size= 8;
+const unsigned int c_inner_border_size= 4;
+
+const unsigned char c_background_color[]= { 32, 32, 32 };
+const unsigned char c_inactive_aperture_color[]= { 200, 190, 170 };
+const unsigned char c_active_aperture_color[]= { 150, 148, 130 };
+const unsigned char c_note_name_color[]= { 84, 80, 76 };
+
+// Notes
+bool note_state_table[c_harp_apertures_count][c_harp_aprture_modes_count]=
+{
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false },
+	{ false, false }
+};
+
 } // namespace
 
 static int NearestPowerOfTwoFloor( int x )
@@ -92,12 +122,10 @@ void DeInitAudio()
 
 void InitWindow()
 {
-	int width= 400, height= 200;
-
 	window_= SDL_CreateWindow(
 		"Harp Simulator",
 		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		width, height,
+		c_window_width, c_window_height,
 		0 );
 
 	if( window_ == nullptr )
@@ -120,6 +148,46 @@ void DeInitWindow()
 	SDL_DestroyWindow( window_ );
 }
 
+void Draw()
+{
+	const auto fill_rect=
+	[]( const int x, const int y, const int width, const int height, const unsigned char* const color )
+	{
+		int pitch= surface_->pitch / 4;
+		unsigned char* const dst= static_cast<unsigned char*>(surface_->pixels) + ( x + y * pitch ) * 4;
+		for( int dy= 0; dy < height; ++dy )
+		for( int dx= 0; dx < width ; ++dx )
+		{
+			for( int j= 0; j < 3; ++j )
+				dst[ ( dx + dy * pitch ) * 4 + j ]= color[j];
+			dst[ ( dx + dy * pitch ) * 4 + 3 ]= 128;
+		}
+	};
+
+	fill_rect( 0, 0, surface_->w, surface_->h, c_background_color );
+
+	const unsigned int c_edge_border_size= 8;
+	const unsigned int c_inner_border_size= 2;
+
+	unsigned int window_width=  surface_->w;
+	unsigned int window_height= surface_->h;
+
+	unsigned int plate_width=
+		(window_width - c_edge_border_size*2 - 2 * c_harp_apertures_count * c_inner_border_size )
+		/ c_harp_apertures_count;
+	unsigned int plate_height=
+		(window_height - c_edge_border_size*2 - (c_harp_aprture_modes_count-1) * c_inner_border_size )
+		/ c_harp_aprture_modes_count;
+
+	for( unsigned int x= 0; x< c_harp_apertures_count; x++ )
+	for( unsigned int y= 0 ;y< c_harp_aprture_modes_count; y++ )
+	{
+		unsigned int r_x= c_edge_border_size + (plate_width + c_inner_border_size * 2) * x;
+		unsigned int r_y= c_edge_border_size + (plate_height + c_inner_border_size * 2) * y;
+		fill_rect( r_x, r_y, plate_width, plate_height, note_state_table[x][y] ? c_active_aperture_color : c_inactive_aperture_color );
+	}
+}
+
 void MainLoop()
 {
 	while(true)
@@ -139,11 +207,46 @@ void MainLoop()
 			};
 		};
 
+		{
+			int key_count;
+			const Uint8* const keyboard_state= SDL_GetKeyboardState( &key_count );
+
+			for( int i= 0; i < key_count; i++ )
+			{
+				const int code= SDL_Scancode(i);
+				if( code >= SDL_SCANCODE_1 && code <= SDL_SCANCODE_9 )
+					note_state_table[ code - SDL_SCANCODE_1 ][0]= keyboard_state[i] != 0;
+				else if( code == SDL_SCANCODE_0 )
+					note_state_table[9][0]= keyboard_state[i] != 0;
+
+				static const int c_code_to_aperture_number[c_harp_apertures_count]=
+				{
+					SDL_SCANCODE_Q,
+					SDL_SCANCODE_W,
+					SDL_SCANCODE_E,
+					SDL_SCANCODE_R,
+					SDL_SCANCODE_T,
+					SDL_SCANCODE_Y,
+					SDL_SCANCODE_U,
+					SDL_SCANCODE_I,
+					SDL_SCANCODE_O,
+					SDL_SCANCODE_P,
+				};
+				for( int j= 0; j < c_harp_apertures_count; ++j )
+					if( code == c_code_to_aperture_number[j] )
+						note_state_table[j][1]= keyboard_state[i] != 0;
+			}
+		}
+
 		if( SDL_MUSTLOCK( surface_ ) )
 			SDL_LockSurface( surface_ );
 
+		Draw();
+
 		if( SDL_MUSTLOCK( surface_ ) )
 			SDL_UnlockSurface( surface_ );
+
+		SDL_UpdateWindowSurface( window_ );
 	}
 }
 
